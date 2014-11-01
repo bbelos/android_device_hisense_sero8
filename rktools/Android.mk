555609d97f7a57672f750d9906aff37b3ae57e2a
Brian Beloshapka <bbelos@hotmail.com>
2014-11-01 12:44:47 -0400

    Add rkcrc from rktools

---
+++ rktools/Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH:= $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),sero8)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := rkcrc.c
+LOCAL_STATIC_LIBRARIES := libmincrypt
+LOCAL_MODULE := rkcrc
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_HOST_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := rkcrc.c
+LOCAL_STATIC_LIBRARIES := libmincrypt libcutils libc
+LOCAL_MODULE := rkcrc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_STEM := rkcrc
+LOCAL_MODULE_CLASS := UTILITY_EXECUTABLES
+LOCAL_UNSTRIPPED_PATH := $(PRODUCT_OUT)/symbols/utilities
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+include $(BUILD_EXECUTABLE)
+
+endif
