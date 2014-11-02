238c59484c060e9004204ff885e3c5f141061c0b
Brian Beloshapka <bbelos@hotmail.com>
2014-11-02 11:47:13 -0500

    Add stock ueventd.rk30board.rc

--- sero8.mk
+++ sero8.mk
@@ -38,6 +38,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/root/fstab.rk30board:root/fstab.rk30board \
     $(LOCAL_PATH)/root/init.rk30board.rc:root/init.rk30board.rc \
+    $(LOCAL_PATH)/root/ueventd.rk30board.rc:root/ueventd.rk30board.rc \
     $(LOCAL_PATH)/root/rk30xxnand_ko.ko.3.0.36+:root/rk30xxnand_ko.ko.3.0.36+
 
 PRODUCT_CHARACTERISTICS := tablet
