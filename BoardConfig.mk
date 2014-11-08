6529e2d9a0f2a8e023912eb384e0956fe1891240
Brian Beloshapka <bbelos@hotmail.com>
2014-11-08 15:23:17 -0500

    sero8: BoardConfig: wifi flags
    Change-Id: I86c426d3d88817433c20930853372728c04aa684

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,6 +54,17 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_USERIMAGES_USE_EXT4 := true
 
+# Wi-Fi
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE := bcmdhd
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA := "/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP := "/vendor/firmware/fw_bcmdhd_apsta.bin"
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/root/fstab.rk30board
 RECOVERY_FSTAB_VERSION := 2
