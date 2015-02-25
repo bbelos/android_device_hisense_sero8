421c4ac6e63736e591f7650c1e0cb0d282e5f31d
Brian Beloshapka <bbelos@hotmail.com>
2015-02-24 21:57:23 -0500

    sero8: add BDROID_BUILDCFG
    Change-Id: I30aaf0afa67982784e674818f489c4e5f1150cef

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,6 +68,7 @@ WIFI_DRIVER_FW_PATH_AP := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
 BOARD_BLUEDROID_VENDOR_CONF := $(LOCAL_PATH)/bluetooth/vnd_rockchip.txt
 
 # Recovery
